d8c63adfb59b59714a1d6c51f2bba259ec7c1a96
Jose Ramos <engrjvramos@gmail.com>
2022-03-04 12:28:52 +0800

    update: finished contact.html inc. media queries

--- script.js
+++ script.js
@@ -61,3 +61,82 @@ window.addEventListener('resize', () => {
 		document.body.classList.remove('resize-animation-stopper');
 	}, 400);
 });
+
+// CONTACT FORM
+
+const form = document.getElementById('contact--form');
+const submitBtn = document.getElementById('submit-btn');
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const phoneInput = document.getElementById('phone');
+const messageInput = document.getElementById('message');
+
+const showError = (input, message) => {
+	const errorContainer = input.nextElementSibling;
+	const errorMsg = errorContainer.querySelector('.error-msg');
+
+	errorContainer.classList.add('active');
+	errorMsg.innerText = message;
+};
+
+const removeError = (input) => {
+	const errorContainer = input.nextElementSibling;
+
+	errorContainer.classList.remove('active');
+};
+
+const validateEmail = (email) => {
+	return String(email)
+		.toLowerCase()
+		.match(
+			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+		);
+};
+
+function checkName() {
+	if (nameInput.value === '') {
+		showError(nameInput, `Can't be empty`);
+	} else {
+		removeError(nameInput);
+	}
+}
+
+function checkEmail() {
+	if (emailInput.value === '') {
+		showError(emailInput, `Can't be empty`);
+	} else if (!validateEmail(emailInput.value)) {
+		showError(emailInput, 'Please use a valid email address');
+	} else {
+		removeError(emailInput);
+	}
+}
+
+function checkMessage() {
+	if (messageInput.value === '') {
+		showError(messageInput, `Can't be empty`);
+	} else {
+		removeError(messageInput);
+	}
+}
+
+if (nameInput) {
+	nameInput.addEventListener('input', checkName);
+}
+
+if (emailInput) {
+	emailInput.addEventListener('input', checkEmail);
+}
+
+if (messageInput) {
+	messageInput.addEventListener('input', checkMessage);
+}
+
+if (form) {
+	form.addEventListener('submit', (e) => {
+		e.preventDefault();
+
+		checkName();
+		checkEmail();
+		checkMessage();
+	});
+}
